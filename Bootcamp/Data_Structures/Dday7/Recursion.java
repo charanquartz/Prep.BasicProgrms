b45d6cc7d99699b1193b7b1fbf5b644239ecb26a
Charan V <charanquartzzz@gmail.com>
2022-09-27 14:02:54 +0000

    installation

---
+++ Bootcamp/Data_Structures/Dday7/Recursion.java
@@ -0,0 +1,13 @@
+package Bootcamp.Data_Structures.Dday7;
+
+import java.util.Scanner;
+
+public class Recursion {
+    public static void main(String args){
+        Scanner r = new Scanner(System.in);
+        int n = r.nextInt();
+        System.out.println(n);
+
+    }
+    
+}
